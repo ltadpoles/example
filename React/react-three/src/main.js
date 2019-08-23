3b1d782b1d314159b26f80ad35eb587f48b91573
tadpole <ltadpole@163.com>
2019-08-23 18:11:54 +0800

    React

--- React/react-three/src/main.js
+++ React/react-three/src/main.js
@@ -1,17 +1,42 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-
 class Dv extends React.Component {
     constructor(props) {
+        console.warn('组件生命周期：constructor')
         super(props)
+        this.state = { name: 'tadpole', age: 17 }
+    }
+
+    // static getDerivedStateFromProps(props, state) {
+    //     console.warn('组件生命周期：getDerivedStateFromProps')
+    //     if (props.age !== state.age) {
+    //         return {
+    //             name: 'tadpole',
+    //             age: props.age
+    //         }
+    //     }
+    //     return null
+    // }
+    UNSAFE_componentWillMount() {
+        console.warn('组件生命周期：UNSAFE_componentWillMount')
     }
 
     render() {
+        console.warn('组件生命周期：render')
         return (
-            <div>test</div>
+            <div>Hello,我叫{this.state.name},今年{this.state.age}岁了</div>
         )
     }
+
+    componentDidMount() {
+        console.warn('组件生命周期：componentDidMount')
+        this.setState({
+            name: 'ltadpole',
+            age: 19
+        })
+    }
+    
 }
 
-ReactDom.render(<Dv />, document.getElementById('app'))
+ReactDom.render(<Dv age={18}/>, document.getElementById('app'))
