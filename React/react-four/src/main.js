523a77d268ccc31409e2118af03e98e8da20fc1e
tadpole <ltadpole@163.com>
2019-09-03 18:27:53 +0800

    React

--- React/react-four/src/main.js
+++ React/react-four/src/main.js
@@ -1,19 +1,63 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
+// 评论组件
 
-class Dv extends React.Component {
+class CommentData extends React.Component {
     constructor(props) {
         super(props)
     }
-    getTxt = () => {
-        console.log(123)
-    }
+
     render() {
         return (
-            <div>test</div>
+            <div>
+                <textarea rows='6' cols='50'></textarea>
+                <br />
+                <button>发表评论</button>
+            </div>
         )
     }
 }
 
-ReactDom.render(<Dv />, document.getElementById('app'))
+// 列表显示组件
+function CommentList(props) {
+    return (
+        <ul>
+            {
+                props.list.map((res, index) => {
+                    return (
+                        <li key={index}>
+                            <p>{res.name}</p>
+                            <p>{res.data}</p>
+                        </li>
+                    )
+                })
+            }
+        </ul>
+    )
+}
+
+
+class Comment extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            list: [
+                {name: '小二', data: '这个好棒啊！'},
+                {name: 'lucky', data: '我也是这么觉得'}
+            ]
+        }
+    }
+    render() {
+        let commentLists = <CommentList  list={this.state.list}/>
+        let commentData = <CommentData />
+        return (
+            <div>
+                <div>{commentData}</div>
+                <div>{commentLists}</div>
+            </div>
+        )
+    }
+}
+
+ReactDom.render(<Comment />, document.getElementById('app'))
