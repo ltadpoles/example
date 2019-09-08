fa6ace3a898f0f2ec3a3ca2fe20bb377bf89e98f
tadpole <ltadpole@163.com>
2019-09-08 14:54:40 +0800

    React

--- React/react-communication/src/main.js
+++ React/react-communication/src/main.js
@@ -1,54 +1,81 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-
-class App extends React.Component {
+// 评论输入组件
+class CommentData extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {
-            data: ''
-        }
+        this.state = { data: '123' }
     }
-    // 自定义的回调事件
-    childValue = data => {
-        this.setState({
-            data
-        })
-    }
-    render() {
-        return (
-            <div>
-                子组件传递过来的值：{this.state.data}
-                <Child transferValue={this.childValue}/>
-            </div>
-            
-        )
-    }
-}
 
-// 子组件
-class Child extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            data: ''
-        }
-    }
-    valueChange = data => {
-        // 保证子组件中的值和传递过去的值一致
+    commentChange = (data) => {
         this.setState({
             data: data.target.value
         })
-        // 触发回调 传递给父组件
-        this.props.transferValue(data.target.value)
     }
+
+    submit = () => {
+        this.props.newList(this.state)
+    }
+
     render() {
         return (
             <div>
-                子组件： <input vlaue={this.state.data} onChange={this.valueChange} />
+                <textarea rows='6' cols='50' value={this.state.data} onChange={this.commentChange}></textarea>
+                <br />
+                <button onClick={this.submit}>发表评论</button>
             </div>
         )
     }
 }
 
-ReactDom.render(<App />, document.getElementById('app'))
+// 评论列表显示组件
+function CommentList(props) {
+    return (
+        <ul>
+            {
+                props.list.map((res, index) => {
+                    return (
+                        <li key={index}>
+                            <p>用户{index + 1}</p>
+                            <p>{res.data}</p>
+                        </li>
+                    )
+                })
+            }
+        </ul>
+    )
+}
+
+// 父组件
+class Comment extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            title: 'commemt',
+            list: [
+                {data: '这个好棒啊！'},
+                {data: '我也是这么觉得'}
+            ]
+        }
+    }
+    listChange = data => {
+        this.setState((state, props) => {
+            let list = state.list.push(data)
+            return list
+        })
+    }
+    render() {
+        let commentLists = <CommentList  list={this.state.list} />
+        let commentData = <CommentData newList={this.listChange} />
+        return (
+            <div>
+                <h1>{this.state.title}</h1>
+                <div>{commentData}</div>
+                <div>{commentLists}</div>
+            </div>
+        )
+    }
+}
+
+ReactDom.render(<Comment />, document.getElementById('app'))
