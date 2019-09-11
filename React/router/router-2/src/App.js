c1891d3d5ec15cdaca327424b370969ac17c9e55
tadpole <ltadpole@163.com>
2019-09-11 23:18:07 +0800

    React-router

---
+++ React/router/router-2/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <Router>
+
+            </Router>
+         );
+    }
+}
+ 
+export default App;
