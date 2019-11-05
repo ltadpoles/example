06e3f946fbb0a118f59f239b07b4c815b11caa01
tadpole <ltadpole@163.com>
2019-11-05 23:10:14 +0800

    React

--- React/hook/src/App.js
+++ React/hook/src/App.js
@@ -3,9 +3,10 @@ import React, { useState, useEffect } from 'react'
 function App(props) {
     // Hook 是一些可以让你在函数组件里“钩入” React state 及生命周期等特性的函数。Hook 不能在 class 组件中使用
     // 只能在函数最外层调用 Hook。不要在循环、条件判断或者子函数中调用
-    // 只能在 React 的函数组件中调用 Hook。不要在其他 JavaScript 函数中调用
+    // 只能在 React 的函数组件或自定义 Hook 中调用 Hook。不要在其他 JavaScript 函数中调用
     // Hook 是一种复用状态逻辑的方式，它不复用 state 本身,Hook 的每次调用都有一个完全独立的 state
     // Hook 使用了 JavaScript 的闭包机制
+    // 
 
     // 自定义 Hook
     // 如果函数的名字以 “use” 开头并调用其他 Hook，我们就说这是一个自定义 Hook
@@ -14,10 +15,9 @@ function App(props) {
     // useState 是一种新方法，它与 class 里面的 this.state 提供的功能完全相同
 
     // React Hooks不能出现在条件判断语句中，因为它必须有完全一样的渲染顺序
+
     // 可以多次声明
     let [count, setCount] = useState(2)
-    let [fruit, setFruit] = useState('banana')
-    let [todos, setTodos] = useState([{ text: 'Learn Hooks' }])
 
     // 副作用：在 React 组件中执行数据获取、订阅或者手动修改 DOM。我们统一把这些操作称为“副作用”，或者简称为“作用”
 
@@ -28,6 +28,7 @@ function App(props) {
 
     // 每次状态发生变化，useEffect都进行了解绑
     // 可以使用 useEffect 的第二个参数，数组中可以写入很多状态对应的变量，当状态值发生变化时，我们才进行解绑。但是当传空数组[]时，就是当组件将被销毁时才进行解绑
+    // Hook 允许我们按照代码的用途分离他们， 而不是像生命周期函数那样。React 将按照 effect 声明的顺序依次调用组件中的每一个 effect。
 
     useEffect(() => {
         // 在函数组件中，我们没有 this，所以我们不能分配或读取 this.state
@@ -40,6 +41,22 @@ function App(props) {
             console.log('解绑了')
         }
     })
+
+    // 第二个参数可用于性能优化 ，接收一个数组，只要数组中的值发生了变化，那么就会执行这个 useEffect 反之则不执行，但不影响首次渲染执行
+    // 如果想执行只运行一次的 effect（仅在组件挂载和卸载时执行），可以传递一个空数组（[]）作为第二个参数
+    let [fruit, setFruit] = useState('banana')
+    useEffect(() => {
+        console.log(`这是另一个 useEffect,我叫${fruit}`)
+    }, [fruit])
+
+    let [todos, setTodos] = useState([{ text: 'Learn Hooks' }])
+    useEffect(() => {
+        // effect 内部的 props 和 state 就会一直拥有其初始值
+        count++ // 这里还是会影响全局的 count，但是 React 会等待浏览器完成画面渲染之后才会延迟调用 useEffect，所以首次渲染的还是2
+        console.log(`这个只是在初始渲染和卸载的时候触发====${count}`)
+        
+    }, [])
+
 
     return (
         <div>
