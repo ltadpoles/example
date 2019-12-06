44b644f7a9bd85b3b3dcf1abd8c6daaac0ceaa58
tadpole <ltadpole@163.com>
2019-12-06 11:13:40 +0800

    ES6

--- ES6/ES6(一).js
+++ ES6/ES6(一).js
@@ -136,3 +136,27 @@
 
 // // 报错 箭头函数没有 this
 // // 由于对象不能构成单独的作用域，多以箭头函数中的 this 是指向全局的
+
+// // 新增与 indexof 类似的方法
+// let b = 'abcd'
+
+// b.includes('a') // true
+// b.startsWith('a') // true
+// b.endsWith('c') // false
+// b.endsWith('d') // true
+
+// // 字符串重复方法
+
+// let a = 'tadpole'
+// a.repeat(2) // tadpoletadpole
+
+// // isNaN 和 Number.isNaN 的区别
+
+// isNaN(123)  // false
+// Number.isNaN(123) // false
+
+// isNaN(NaN) // true
+// Number.isNaN(NaN) //true
+
+// isNaN('abc')  // true
+// Number.isNaN('abc') // false
