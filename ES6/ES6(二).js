9445b84b7b9539036a12c38f84bd66dea559c0b7
tadpole <ltadpole@163.com>
2019-12-09 18:29:33 +0800

    ES6

---
+++ ES6/ES6(二).js
@@ -0,0 +1,17 @@
+/**
+ * 递归实现数组扁平化
+ * @param {原数组} arr 
+ */
+function flatten(arr) {
+    let result = []
+    for(let i = 0; i < arr.length; i++) {
+        if(Array.isArray(arr[i])) {
+            result = result.concat(flatten(arr[i]))
+        } else {
+            result.push(arr[i])
+        }
+    }
+    return result
+}
+
+flatten([1, 2, [3, [4, 5]]]) // [1, 2, 3, 4, 5]
