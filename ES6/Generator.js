95f6ee71d53ec00369409fc2d22458aaaedd0e60
tadpole <ltadpole@163.com>
2019-12-18 11:41:50 +0800

    ES6

--- ES6/Generator.js
+++ ES6/Generator.js
@@ -17,7 +17,59 @@ function* gen() {
 
 let n = gen()
 
-console.log(n.next())
-console.log(n.next())
+// console.log(n.next())
+// console.log(n.next())
 
 // 不能直接使用 gen().next()  这样一直是同一个结果`
+
+
+// function* fn() {
+//     yield 'hello'
+//     yield 'world'
+//     return 'stop'
+// }
+
+// function* fn1() {
+//     yield 'fn1'
+//     yield* fn()
+// }
+
+// function* fn2() {
+//     yield 'start'
+//     yield* fn1()
+//     yield 'end'
+// }
+
+// for(let i of fn2()) {
+//     console.log(i)
+// }
+
+// 
+
+// let go = fn2()
+
+// console.log(go.next())
+// console.log(go.next())
+// console.log(go.next())
+// console.log(go.next())
+// console.log(go.next())
+// console.log(go.next())
+
+
+function* fn() {
+    try {
+        yield 'hello'
+        yield 'world'
+    } catch(e) {
+        console.log(e)
+    }
+}
+
+let go = fn()
+
+console.log(go.next())
+
+go.throw('出错了!')
+
+console.log(go.next())
+console.log(123)
