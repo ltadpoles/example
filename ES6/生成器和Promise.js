a58145cb948babb9667917d0584823d4c5f9c079
tadpole <ltadpole@163.com>
2019-12-24 22:38:12 +0800

    ES6

---
+++ ES6/生成器和Promise.js
@@ -0,0 +1,24 @@
+function foo() {
+    return Promise.resolve(2)
+}
+
+function* main() {
+    try {
+        let text = yield foo()
+        // console.log(text)
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+let it = main()
+let p = it.next().value
+
+console.log(p)
+
+p.then(res => {
+    console.log(res)
+    it.next(res)
+}, err => {
+    console.log(err)
+})
