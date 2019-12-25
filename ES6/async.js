b9d6f41215ef44abe51f86dfaa4625c30b8cea71
tadpole <ltadpole@163.com>
2019-12-25 18:08:55 +0800

    ES6

--- ES6/async.js
+++ ES6/async.js
@@ -1,23 +1,34 @@
-function getName() {
-    return '游荡de蝌蚪'
-}
+// function getName() {
+//     return '游荡de蝌蚪'
+// }
 
-function getAge() {
-    return 18
-}
+// function getAge() {
+//     return 18
+// }
 
-async function getInfo() {
-    // let name = await getName()
-    // let age = await getAge()
+// async function getInfo() {
+//     // let name = await getName()
+//     // let age = await getAge()
 
-    let [name, age] = await Promise.all([getName(), getAge()])
+//     let [name, age] = await Promise.all([getName(), getAge()])
     
-    return {
-        name,
-        age
-    }
+//     return {
+//         name,
+//         age
+//     }
+// }
+
+// getInfo().then(res => {
+//     console.log(res)
+// })
+
+async function getName() {
+    return Promise.resolve(2)
 }
 
-getInfo().then(res => {
-    console.log(res)
-})
+async function say() {
+    let name = await getName()
+    console.log(name)
+}
+
+say()
