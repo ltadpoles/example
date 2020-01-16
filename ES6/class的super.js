ec5b89d84ce016f052a94e269f7ecf883bb8905f
tadpole <ltadpole@163.com>
2020-01-16 21:55:48 +0800

    ES6

--- ES6/class的super.js
+++ ES6/class的super.js
@@ -1,23 +1,61 @@
+// class Person {
+//     constructor() {
+//         this.name = '游荡de蝌蚪'
+//     }
+// }
+// class Tadpole extends Person {
+//     constructor(){
+//         // super()
+//         console.log(super())
+//     }
+// }
+
+// class Tadpole223 extends Tadpole {
+//     constructor() {
+//         // super 实际返回的是当前类。添加了 this
+//         console.log(super())
+//     }
+// }
+
+// let t = new Tadpole223()
+
+// console.log(t.name)
+
 class Person {
     constructor() {
         this.name = '游荡de蝌蚪'
     }
+    say() {
+        console.log('My name is' + this.name)
+    }
+    // static say() {
+    //     console.log('My name in tadpole')
+    // }
 }
+
+// class Tadpole extends Person {
+//     constructor() {
+//         super()
+//         console.log(super.say())
+//         console.log(super.name)
+//     }
+// }
+
+// let t = new Tadpole()
+
 class Tadpole extends Person {
-    constructor(){
-        // super()
-        console.log(super())
+    static say() {
+        super.say()
+    }
+    say() {
+        super.say()
     }
 }
 
-class Tadpole223 extends Tadpole {
-    constructor() {
-        // super 实际返回的是当前类。添加了 this
-        console.log(super())
-    }
-}
+// Person.say()
 
+Tadpole.say()
 
-let t = new Tadpole223()
+let t = new Tadpole()
 
-console.log(t.name)
+t.say()
