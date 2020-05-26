89adf34465ba1f1a03fc56e9eb2de9b99946581d
tadpole <ltadpole@163.com>
2020-05-26 12:51:35 +0800

    JavaScript

---
+++ JavaScript/函数柯里化.js
@@ -0,0 +1,32 @@
+// 简单来说 就是逐步收集参数
+function curry(fn) {
+    let args = Array.prototype.slice.call(arguments, 1)
+    return function() {
+        let _args = Array.prototype.slice.call(arguments)
+        let final = args.concat(_args)
+        return fn.apply(null, final)
+    }
+}
+
+
+function add() {
+    // 第一次执行时，定义一个数组专门用来存储所有的参数
+    var _args = Array.prototype.slice.call(arguments);
+
+    // 在内部声明一个函数，利用闭包的特性保存_args并收集所有的参数值
+    var _adder = function() {
+        _args.push(...arguments);
+        return _adder;
+    };
+
+    // 利用toString隐式转换的特性，当最后执行时隐式转换，并计算最终的值返回
+    _adder.toString = function () {
+        return _args.reduce(function (a, b) {
+            return a + b;
+        });
+    }
+    return _adder;
+}
+console.log(add(1))
+alert
+add(1)
