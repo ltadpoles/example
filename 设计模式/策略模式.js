ea7fe963a3ff8130ea17eb35a84955304fb7fddd
tadpole <ltadpole@163.com>
2020-06-08 23:18:38 +0800

    JavaScript

---
+++ 设计模式/策略模式.js
@@ -0,0 +1,34 @@
+// 定义：定义一系列的算法，把他们一个个封装起来，并且可以使他们相互替换（函数参数）
+// 目的：将算法的使用与算法的实现分离开来
+// 一般分为两个部分：策略类 ==> 封装具体的算法  环境类 ==> 接受用户的请求，将请求委托给某一个策略类（）
+
+// 绩效计算工资 S A B C
+
+/**
+ * 绩效计算初始版
+ * 
+ * @param {String} level 绩效等级
+ * @param {Number} salary 工资基数
+ */
+var calculateBonus = function (level, salary) {
+    if(level === 'S') {
+        return salary * 4
+    }
+    if(level === 'A') {
+        return salary * 3
+    }
+    if(level === 'B') {
+        return salary * 2
+    }
+    if(level === 'C') {
+        return salary
+    }
+}
+
+var tadpole = calculateBonus('A', 10000) // 30000
+var ltadpole = calculateBonus('C', 14000) // 14000
+
+// 缺点：
+// 1. 函数 calculateBonus 比较庞大，包含了很多 if-else 判断
+// 2. 缺乏弹性，不利于增加修改逻辑 违反开放-封闭原则
+// 3. 复用性差
