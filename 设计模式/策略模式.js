8ace41fac57d18f00fb7c6791c0c5db1ee872856
tadpole <ltadpole@163.com>
2020-06-08 23:24:24 +0800

    JavaScript

--- 设计模式/策略模式.js
+++ 设计模式/策略模式.js
@@ -4,31 +4,51 @@
 
 // 绩效计算工资 S A B C
 
-/**
- * 绩效计算初始版
- * 
- * @param {String} level 绩效等级
- * @param {Number} salary 工资基数
- */
-var calculateBonus = function (level, salary) {
-    if(level === 'S') {
-        return salary * 4
-    }
-    if(level === 'A') {
-        return salary * 3
-    }
-    if(level === 'B') {
-        return salary * 2
-    }
-    if(level === 'C') {
-        return salary
-    }
-}
+// /**
+//  * 绩效计算初始版
+//  * 
+//  * @param {String} level 绩效等级
+//  * @param {Number} salary 工资基数
+//  */
+// var calculateBonus = function (level, salary) {
+//     if (level === 'S') {
+//         return salary * 4
+//     }
+//     if (level === 'A') {
+//         return salary * 3
+//     }
+//     if (level === 'B') {
+//         return salary * 2
+//     }
+// }
 
-var tadpole = calculateBonus('A', 10000) // 30000
-var ltadpole = calculateBonus('C', 14000) // 14000
+// var tadpole = calculateBonus('A', 10000) // 30000
+// var ltadpole = calculateBonus('B', 14000) // 28000
 
 // 缺点：
 // 1. 函数 calculateBonus 比较庞大，包含了很多 if-else 判断
 // 2. 缺乏弹性，不利于增加修改逻辑 违反开放-封闭原则
 // 3. 复用性差
+
+// 使用策略模式分离逻辑
+
+// 各种算法
+var strategies = {
+    'S': function (salary) {
+        return salary * 4
+    },
+    'A': function (salary) {
+        return salary * 3
+    },
+    'B': function (salary) {
+        return salary * 2
+    }
+}
+// 策略类
+var calculateBonus = function (level, salary) {
+    return strategies[level](salary)
+}
+
+var tadpole = calculateBonus('A', 10000) // 30000
+var ltadpole = calculateBonus('B', 14000) // 28000
+console.log(ltadpole)
