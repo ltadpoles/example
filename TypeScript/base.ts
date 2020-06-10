e59f05554d3f49cc94ffae36b3e9a9bcc874672d
ltadpole <ltadpole@163.com>
2020-06-10 16:23:27 +0800

    TypeScript

--- TypeScript/base.ts
+++ TypeScript/base.ts
@@ -61,6 +61,7 @@ let Tom: Person1 = {
     id: 1
 }
 // Tom.id = 2 // Error
+// 6. 接口与接口之间可以有继承关系，用 extends 关键字
 
 // 函数
 // 1. 函数声明和表达式 参数个数必须是对应的
@@ -96,4 +97,103 @@ let b: number = (person as string).length // 推荐
 // 声明语句
 declare var person1: string
 
+// 类型别名：使用 type 定义
+type Name = string
+type myNumber = number
+type Fn = () => void
 
+// 字符串字面量类型用来约束取值只能是某几个字符串中的一个
+// 类型别名与字符串字面量类型都是使用 type 进行定义
+type Name1 = 'tadpole' | 'ltadpole' | 'Jon'
+
+// 元组：表示一个已知元素数量和类型的数组
+let arr: [number, string]
+arr[0] = 2 // 可以单个赋值
+arr = [1, 'tadpole'] // 当修改变量的时候，必须使数组数量和类型对应
+arr.push(2)
+arr.push('ltadpole')
+arr.push(undefined) // undefined 和 null 是所有类型的子类型
+arr.push(null)
+// arr.push(true) // Error
+
+// 枚举：成员会被赋值从0开始的递增的数字;也可以手动赋值
+enum Days {
+    Sun,
+    Mon,
+    Tue,
+    Wed,
+    Thu,
+    Fri,
+    Sat
+}
+Days.Sun === 0 // true
+Days[0] === 'Sun' // true
+enum Day {
+    fir,
+    sec = 2
+}
+// 1. 常数枚举：使用 const enum 定义的枚举类型，会在编译阶段被删除，不能包含计算成员
+const enum Day1 {
+    fir,
+    sec
+}
+// 2. 外部枚举：使用 declare enum 定义的枚举类型
+declare enum Day2 {
+    fir,
+    sec
+}
+
+// 类
+// 可以使用三种访问修饰符
+// public：公有的属性或方法，可以在任何地方被访问到。默认所有的属性和方法都是 public
+// private: 私有的属性或方法，不能在声明的类外部使用
+// protected：受保护的属性或方法，可以在自身和子类中被访问
+// 当构造函数修饰为 private 时，该类不允许被继承和实例化；修饰为 protected 时，该类只能被继承
+// 抽象类（abstract）：不允许被实例化；抽象类中的抽象方法必须在子类中实现
+// 不同类之间可以有一些共有的特性，使用 implements 关键字来实现(比如说门有个报警功能，车也有一个报警功能)
+interface Alarm {
+    alert(): void
+}
+class Door implements Alarm {
+    alert() {
+        console.log('报警了')
+    }
+}
+class Car implements Alarm {
+    alert() {
+        console.log('报警了')
+    }
+}
+// 同时车还有灯光功能
+interface Light {
+    lightOn(): void
+    lightOff(): void
+}
+
+class Car1 implements Alarm, Light {
+    alert() {
+        console.log('报警了')
+    }
+    lightOn() {
+        console.log('开灯')
+    }
+    lightOff() {
+        console.log('关灯')
+    }
+}
+// 接口可以继承自类
+class Person5 {
+    x: number
+    y: number
+    constructor(x: number, y: number) {
+        this.x = x
+        this.y = y
+    }
+}
+interface Point extends Person5 {
+    z: number
+}
+let point: Point = { x: 1, y: 2, z: 3 }
+
+// 装饰器：装饰器是一个对类进行处理的函数。装饰器函数的第一个参数，就是所要装饰的目标类。
+// 装饰器对类的行为的改变，是代码编译时发生的，而不是在运行时 ==> 装饰器本质就是编译时执行的函数
