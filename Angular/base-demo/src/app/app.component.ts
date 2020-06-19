5c701e6e8e3b3202c16c1458a0a40845e62de853
ltadpole <ltadpole@163.com>
2020-06-19 16:24:24 +0800

    Angular

--- Angular/base-demo/src/app/app.component.ts
+++ Angular/base-demo/src/app/app.component.ts
@@ -1,13 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, SimpleChanges, ViewChild } from '@angular/core'
+import { MessagesComponent } from './messages/messages.component'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  condition: boolean = false;
-  birthday: Date = new Date()
-  color: string;
-  title = 'base-demo';
+    @ViewChild(MessagesComponent) viewChild: MessagesComponent
+    condition: boolean = false
+    birthday: Date = new Date()
+    color: string
+    title = 'base-demo'
+    ngOnInit(): void {
+        //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+        //Add 'implements OnInit' to the class.
+        console.log('init')
+    }
+    ngOnChanges(changes: SimpleChanges): void {
+        //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+        //Add '${implements OnChanges}' to the class.
+        console.log('changes')
+    }
+    ngDoCheck(): void {
+        //Called every time that the input properties of a component or a directive are checked. Use it to extend change detection by performing a custom check.
+        //Add 'implements DoCheck' to the class.
+        console.log('check')
+    }
+    ngAfterContentInit(): void {
+        //Called after ngOnInit when the component's or directive's content has been initialized.
+        //Add 'implements AfterContentInit' to the class.
+        console.log('content init')
+    }
+    ngAfterContentChecked(): void {
+        //Called after every check of the component's or directive's content.
+        //Add 'implements AfterContentChecked' to the class.
+        console.log('content Check')
+    }
+    ngAfterViewInit(): void {
+        //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+        //Add 'implements AfterViewInit' to the class.
+        console.log('view init')
+    }
+    ngAfterViewChecked(): void {
+        //Called after every check of the component's view. Applies to components only.
+        //Add 'implements AfterViewChecked' to the class.
+        console.log(this.viewChild.hero) // 这样就拿到子组件的值了
+        console.log('view check')
+        
+    }
+    ngOnDestroy(): void {
+        //Called once, before the instance is destroyed.
+        //Add 'implements OnDestroy' to the class.
+        console.log('destroy')
+    }
 }
--- Angular/base-demo/src/app/messages/messages.component.ts
+++ Angular/base-demo/src/app/messages/messages.component.ts
@@ -2,16 +2,16 @@ import { Component, OnInit } from '@angular/core'
 import { MessageService } from '../message.service'
 
 @Component({
-  selector: 'app-messages',
-  templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.scss']
+    selector: 'app-messages',
+    templateUrl: './messages.component.html',
+    styleUrls: ['./messages.component.scss']
 })
 export class MessagesComponent implements OnInit {
+    constructor(public messageService: MessageService) {}
 
-  constructor(public messageService: MessageService) { }
+    hero: string = 'tadpole'
 
-  ngOnInit(): void {
-    console.log(this.messageService)
-  }
-
+    ngOnInit(): void {
+        console.log(this.messageService)
+    }
 }
