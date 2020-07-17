aa0de53873ad328e6d3799f1bf3674ef308f1880
ltadpole <ltadpole@163.com>
2020-07-17 18:13:10 +0800

    TypeScript

--- TypeScript/advanced/reload.ts
+++ TypeScript/advanced/reload.ts
@@ -1,5 +1,4 @@
 // 实现重载的方式就是定义好多个相同的 function 接受不同的参数
-
 declare function Test(params: number): number
 declare function Test(params: string): string
 declare function Test(params: boolean): boolean
--- TypeScript/hello-ts/hello.js
+++
@@ -1,4 +0,0 @@
-let Hello = function (name, age) {
-    console.log('Hello, My name is ' + name + ", I'm " + age + ' years old');
-};
-Hello('tadpole', 18);
