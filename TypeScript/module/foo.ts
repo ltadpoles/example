4171f390a8d1a7f57ebaedbc1051f9c018699e04
ltadpole <ltadpole@163.com>
2020-08-21 16:07:33 +0800

    TypeScript

--- TypeScript/module/foo.ts
+++ TypeScript/module/foo.ts
@@ -1,5 +1,6 @@
 // 全局作用域是比较危险的
 const foo: string = 'tadpole'
+
 // 我们可以将其创建为一个本地作用域
 // export 关键字会影响整个文件的作用域
 export const foo1: string = 'tadpole'
@@ -7,7 +8,6 @@ export const foo1: string = 'tadpole'
 export const foo2: string = 'ltadpole'
 
 // 文件检查的路径
-
 /*
  ./node_modules
  ../node_modules
